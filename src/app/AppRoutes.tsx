f16b875294dc0c47f366cbc38a460ba0159c370d
Egor <139051554+EgorGalkinORG@users.noreply.github.com>
2026-01-10 21:06:43 +0200

    Contact data page done

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "./layout";
+import { UserMePage } from "../pages/userMe";
 
 export function AppRoutes() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Layout />}>
-                    
+                	<Route path="user-me/:id" element={<UserMePage />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
